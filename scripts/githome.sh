36022f680bde20a2c54b247e287e66e3c3b8c66c
Matthew Planchard <msplanchard@gmail.com>
2020-10-10 17:42:24 -0500

    fix bootstrap script

--- scripts/githome.sh
+++ scripts/githome.sh
@@ -9,11 +9,11 @@ if [ ! -f ~/.gitignore/ ]; then
     echo ".dotfiles" > ~/.gitignore
 fi
 
-if [ ! -d ~/.gitconfig ]; then
-    git clone --bare git@github.com:mplanchard/githome ~/.gitconfig
-    mkdir -p .config-backup && \
+if [ ! -d ~/.dotfiles/ ]; then
+    git clone --bare git@github.com:mplanchard/githome ~/.dotfiles
+    mkdir -p ~/.config-backup && \
         githome checkout 2>&1 | egrep "\s+\." | awk '{print $1}' | \
-        xargs -I{} mv {} .config-backup/{}
+        xargs -I{} mv {} ~/.config-backup/{}
     githome checkout
     githome config --local status.showUntrackedFiles no
 fi
