642b97674b1bd7a4b4add1e7b96777689e833d9e
Matthew Planchard <msplanchard@gmail.com>
2026-07-05 14:54:40 -0400

    check standalone module

---
+++ home-manager/core.nix
@@ -0,0 +1,7 @@
+{ ... }:
+{
+  home.username = "matthew";
+  home.homeDirectory = "/home/matthew";
+  home.stateVersion = "22.11";
+  programs.home-manager.enable = true;
+}
--- nix/hosts/emplanada/default.nix
+++ nix/hosts/emplanada/default.nix
@@ -3,8 +3,8 @@
   ...
 }:
 {
-  home.username = "matthew";
-  home.homeDirectory = "/home/matthew";
-  home.stateVersion = "22.11";
-  programs.home-manager.enable = true;
+  inputs = [
+    ../../../home-manager/core.nix
+  ];
+
 }
